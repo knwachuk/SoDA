2526a66da057597f62bac2023638fbafecac3bf9
Kelechi Nwachukwu <caesey2001@yahoo.com>
2017-12-13 13:27:40 -0500

    Add setup.py file to module to complete the Python package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='soda',
+      version='2.0',
+      description='',
+      url='https://github.com/msu-evrl/soda',
+      author='Kelechi Nwachukwu',
+      author_email='caesey2001@yahoo.com',
+      license='MIT',
+      packages=['soda'],
+      zip_safe=False)
